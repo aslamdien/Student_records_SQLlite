915bf892719a2bef483e4296205466328355fd09
aslamdien <aslamdien90@gmail.com>
2021-07-23 15:37:06 +0200

    Initial commit

--- app.py
+++ app.py
@@ -41,4 +41,20 @@ def add_new_record():
             conn.close()
             return render_template('result.html', msg=msg)
 
+@app.route('/show-records/', methods=['GET'])
+def show_records():
+    records = []
+    try:
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM students')
+            records = cur.fetchall()
+    except Exception as e:
+        conn.rollback()
+        print("There was an error fetching results from the database.")
+    finally:
+        conn.close()
+        return render_template('records.html', records=records)
+
+
 
